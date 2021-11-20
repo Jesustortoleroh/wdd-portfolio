00e50b5334a2b60f3527c952b9cd5892ab1a29e2
Jesustortoleroh <you@example.com>
2021-11-20 12:01:33 -0400

    Updated

--- week10/TeamActivity/js/index.js
+++ week10/TeamActivity/js/index.js
@@ -1,15 +1,8 @@
-import { getJSON, getLocation } from './utilities.js';
-import QuakeController from './quakeController.js';
+import QuakesController from './QuakesController.js';
+import buildNavigation from './routing.js';
 
-const baseURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+const navElement = document.getElementById('mainNav');
+buildNavigation(navElement);
 
-const controller = new QuakeController("#quakeList")
-
-document.querySelector('#radiusButton').addEventListener('click', () => {
-    console.log("value: ", document.querySelector('#radiusInput').value)
-    if(document.querySelector('#radiusInput').value != '') {
-        controller.radius = document.querySelector('#radiusInput').value
-        controller.getQuakesByRadius()
-    }
-})
-controller.init()
+const myQuakesController = new QuakesController('#quakeList');
+myQuakesController.getQuakesByRadius();
